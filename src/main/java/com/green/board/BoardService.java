4eeb417b3a60462b96c2919c37eb58edd3daf771
Gusuyoung <suyoung606123@gmail.com>
2024-11-11 17:44:29 +0900

    first(1)

--- src/main/java/com/green/board/BoardService.java
+++ src/main/java/com/green/board/BoardService.java
@@ -1,4 +1,33 @@
 package com.green.board;
 
-public class BoardService {
+import com.green.board.model.BoardInsReq;
+import com.green.board.model.BoardSelOneRes;
+import com.green.board.model.BoardSelRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+/*
+    @Service - 빈 등록, 서비스가 로직처리 담당. 로직처리가 있다면 여기서 처리한다.
+               없으면 연결 작업만... 연결작업이 Controller와 Persistence(DB) 연결
+    빈 등록 : 스프링 컨테이너에게 객체 생성을 대리로 맡기는 것
+            기본적으로 싱글톤으로 객체화 (private을 선언하여 딱 하나만 사용할 수 있게 하는것)
+ */
+
+@Service
+@RequiredArgsConstructor
+public class BoardService{
+    private final BoardMapper mapper;
+
+    public int insBoard(BoardInsReq p){
+        return mapper.insBoard(p);
+    }
+
+    public List<BoardSelRes> selResList() {
+        return mapper.selBoardList();
+    }
+
+    public BoardSelOneRes selBoardOne(int p) {
+        return mapper.selBoardOne(p);
+    }
 }
